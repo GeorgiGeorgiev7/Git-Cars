ccb045e6172081e5642fe1da844d806075434b69
Georgi Georgiev <georgingeorgiev7@gmail.com>
2022-01-28 22:00:03 +0200

    Removed redundant files and code comments

--- services/cars.js
+++ services/cars.js
@@ -1,30 +1,5 @@
-// const fs = require('fs/promises');
 const Car = require('../models/Car');
 
-
-//const filepath = './services/data.json';
-/*
-async function read() {
-    try {
-        const file = await fs.readFile(filepath);
-        return JSON.parse(file);
-    } catch (err) {
-        console.error('Database read error');
-        console.error(err);
-        process.exit(1);
-    }
-}
-
-async function write(data) {
-    try {
-        await fs.writeFile(filepath, JSON.stringify(data, null, 2));
-    } catch (err) {
-        console.error('Database write error');
-        console.error(err);
-        process.exit(1);
-    }
-}
-*/
 
 function carViewModel(car) {
     return {
@@ -56,25 +31,6 @@ async function getAll(query) {
 
     const cars = await Car.find(options);
     return cars.map(carViewModel);
-    /*
-    const data = await read();
-    let cars = Object
-        .entries(data)
-        .map(([id, v]) => Object.assign({}, { id }, v));
-
-    if (query.search) {
-        cars = cars.filter(c => c.name.toLocaleLowerCase()
-            .includes(query.search.toLocaleLowerCase()));
-    }
-
-    if (query.from) {
-        cars = cars.filter(c => Number(c.price) >= Number(query.from));
-    }
-
-    if (query.to) {
-        cars = cars.filter(c => Number(c.price) <= Number(query.to));
-    }
-    */
 }
 
 async function getById(id) {
@@ -84,69 +40,21 @@ async function getById(id) {
     } else {
         return undefined;
     }
-    /*
-    const data = await read();
-    const car = data[id];
-
-    if (car) {
-        return Object.assign({}, { id }, car);
-    } else {
-        return undefined;
-    }
-    */
 }
 
 async function createCar(car) {
     const result = new Car(car);
     await result.save();
-    /*
-    const cars = await read();
-    let id = nextId();
-
-    while (cars.hasOwnProperty(id)) {
-        id = nextId();
-    }
-
-    cars[id] = car;
-
-    await write(cars);
-    */
 }
 
 async function deleteById(id) {
     await Car.findByIdAndDelete(id);
-    /*
-    const data = await read();
-
-    if (data.hasOwnProperty(id)) {
-        delete data[id];
-        await write(data);
-    } else {
-        throw new Error('No such ID in database');
-    }
-    */
 }
 
 async function editById(id, car) {
     await Car.findByIdAndUpdate(id, car);
-    /*
-    const data = await read();
-
-    if (data.hasOwnProperty(id)) {
-        data[id] = car;
-        await write(data);
-    } else {
-        throw new Error('No such ID in database');
-    }
-    */
 }
 
-/*
-function nextId() {
-    return 'xxxxxxxx-xxxx'.replace(/x/g, () =>
-        (Math.random() * 16 | 0).toString(16));
-}
-*/
 
 module.exports = () => (req, res, next) => {
     req.storage = {
